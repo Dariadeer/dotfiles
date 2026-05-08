8238db719c3d1df2cfa409fd01225f3b3f220e7b
dariadeer <butanton2006@gmail.com>
2026-05-08 22:54:38 +0100

    add .bashrc

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+# .config files
 ln -s ~/.myconfig/nvim ~/.config/nvim
 ln -s ~/.myconfig/alacritty ~/.config/alacritty
 ln -s ~/.myconfig/starship.toml ~/.config/starship.toml
 ln -s ~/.myconfig/tmux ~/.config/tmux
+
+# $HOME files
+ln -s ~/.myconfig/.bashrc ~/.bashrc
